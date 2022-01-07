e8f24070b33f81a59a83f8f56330ffbdc1da7ab7
jihoon <ljhljh211@naver.com>
2022-01-07 22:01:14 +0900

    끝말잇기 실습중

---
+++ zero-basic/word-relay.js
@@ -0,0 +1,9 @@
+const number = parseInt(prompt('몇 몇이 참가하나요?'), 10);
+console.log(number);
+// alert(number);
+const yesOrNo = confirm('맞나요?');
+
+const buttonElem = document.querySelector('#keywords');
+console.log(buttonElem);
+
+// document.querySelectorAll('li'); 유사 배열, NodeList 
